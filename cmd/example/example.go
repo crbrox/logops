322270761e33f8c8c463cfda884c80e1a7ae39d2
crbrox <carlos.romerobrox@telefonica.com>
2016-03-03 21:49:02 +0100

    Fix cmd/example

--- cmd/example/example.go
+++ cmd/example/example.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	l := logops.NewLogger(nil)
-	l.Info("%d y %d son %d", 2, 2, 4)
+	l.Infof("%d y %d son %d", 2, 2, 4)
 	l.Info("y ocho dieciséis")
 	l.Context = logops.C{"prefix": "prefijo"}
-	l.InfoC(logops.C{"local": "llamada 1"}, "%d y %d son %d", 2, 2, 4)
-	l.InfoC(logops.C{"local": "llamada 2"}, "y ocho dieciséis")
+	l.InfoC(logops.C{"local": "España y olé"}, "%d y %d son %d", 2, 2, 4)
+	l.InfoC(logops.C{"local": `{"json":"pompón"}`}, "y ocho dieciséis")
 
 }
